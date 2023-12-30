7ca3c4561b14ff1748d533819aca99834a48a520
guanwanxiao <18257132004@163.com>
2023-12-30 20:31:03 +0800

    feat: 测试用例及环境

--- test/create/create.test.ts
+++ test/create/create.test.ts
@@ -1,17 +1,29 @@
 import "reflect-metadata";
-import { DataSource } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, DataSource } from "typeorm";
 import { User } from "../../src/entity/User";
 
-export const AppDataSource = new DataSource({
-  type: "sqlite",
-  database: "database.sqlite",
-  synchronize: true,
-  logging: false,
-  entities: [User],
-  migrations: [],
-  subscribers: [],
-});
-
-AppDataSource.initialize().then(async function () {
-  console.log(AppDataSource);
+describe("Database creation", () => {
+  let AppDataSource: DataSource;
+  beforeAll(async () => {
+    AppDataSource = new DataSource({
+      type: "sqlite",
+      database: "./database/database.sqlite",
+      synchronize: true,
+      logging: false,
+      entities: [User],
+      migrations: [],
+      subscribers: [],
+    });
+    await AppDataSource.initialize();
+  });
+  test("create table", async () => {
+    const user = new User();
+    user.firstName = "Timber";
+    user.lastName = "Saw";
+    user.age = 25;
+    await AppDataSource.manager.save(user);
+    const columns = await AppDataSource.manager.connection.getMetadata(User)
+      .columns;
+    console.log(columns);
+  });
 });
