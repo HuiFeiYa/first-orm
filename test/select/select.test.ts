5b800f4d93149ca914529eb1ba04017ce3fcd5d1
guanwanxiao <18257132004@163.com>
2024-01-02 22:52:38 +0800

    feat: select 测试用例

--- test/select/select.test.ts
+++ test/select/select.test.ts
@@ -6,8 +6,35 @@ import {
   LessThanOrEqual,
   And,
   Between,
+  LessThan,
+  Not,
+  getRepository,
 } from "typeorm";
 import { User } from "../create/User";
+/**
+ * find 完整示例
+ * userRepository.find({
+    select: ["firstName", "lastName"],
+    relations: ["profile", "photos", "videos"],
+    where: {
+        firstName: "Timber",
+        lastName: "Saw"
+    },
+    order: {
+        name: "ASC",
+        id: "DESC"
+    },
+    skip: 5,
+    take: 10,
+    cache: true
+});
+ */
+
+
+/**
+ * 进阶选项
+ * Not、LessThan、MoreThan、Like、Between、In、IsNull 等函数
+ */
 
 let connection: DataSource;
 describe("ORDER BY、GROUP BY、LIMIT、 DISTINCT、AND、OR、NOT、Like", () => {
@@ -48,5 +75,76 @@ describe("ORDER BY、GROUP BY、LIMIT、 DISTINCT、AND、OR、NOT、Like", () =
       ],
     });
     expect(result.map((item) => item.id)).toEqual([2, 3]);
+  });
+  test("OR", async () => {
+    const result = await connection.manager.find(User, {
+      where: [
+        {
+          age: LessThan(15),
+        },
+        {
+          firstName: "Bob",
+        },
+      ],
+    });
+    expect(result.map((item) => item.id)).toEqual([1, 2]);
+  });
+
+  test("Not", async () => {
+    const result = await connection.manager.find(User, {
+      where: [
+        {
+          age: Not(15),
+        },
+      ],
+    });
+    expect(result.map((item) => item.id)).toEqual([1, 2]);
+  });
+
+  test("ORDER BY age ASC", async () => {
+    const userRepository = getRepository(User);
+    const result = await userRepository.find({
+      order: {
+        age: "ASC",
+      },
+    });
+    expect(result.map((item) => item.id)).toEqual([1, 3, 2]);
+  });
+
+  test("ORDER BY age DESC", async () => {
+    const userRepository = getRepository(User);
+    const result = await userRepository.find({
+      order: {
+        age: "DESC",
+      },
+    });
+    expect(result.map((item) => item.id)).toEqual([2, 3, 1]);
+  });
+
+  test("select field", async () => {
+    const userRepository = getRepository(User);
+    const result = await userRepository.find({
+      select: ["id", "age"],
+      order: {
+        age: "ASC",
+      },
+    });
+    expect(result).toEqual([
+      { id: 1, age: 12 },
+      { id: 3, age: 15 },
+      { id: 2, age: 18 },
+    ]);
+  });
+
+  test("max age", async () => {
+    const userRepository = getRepository(User);
+    const result = await userRepository.find({
+      select: ["id", "age"],
+      order: {
+        age: "desc",
+      },
+      take: 1,
+    });
+    expect(result).toEqual([{ id: 2, age: 18 }]);
   });
 });
